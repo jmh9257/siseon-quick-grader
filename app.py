65ec09a987d3f0a894ffcabae693696489cd536d
jmh9257 <jmh9257@naver.com>
2026-08-13 21:37:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -610,7 +610,7 @@ with tab_submit:
     with st.form("submit_form"):
         c1, c2, c3, c4 = st.columns([1.1, 0.8, 1.1, 1.2])
         with c1:
-            nickname = st.text_input("닉네임", placeholder=" ")
+            nickname = st.text_input("닉네임", placeholder="성적 확인 시 필요합니다.")
         with c2:
             exam_round = st.selectbox(
     "시험 회차",
@@ -676,7 +676,7 @@ with tab_edit:
     with st.form("load_edit_form"):
         c1, c2, c3 = st.columns([1.2, 0.8, 1.2])
         with c1:
-            edit_nickname = st.text_input("닉네임", placeholder=" ", key="edit_nickname")
+            edit_nickname = st.text_input("닉네임", placeholder="성적 확인 시 필요합니다.", key="edit_nickname")
         with c2:
             edit_round = st.selectbox(
     "시험 회차",
@@ -770,7 +770,7 @@ with tab_edit:
 
 with tab_records:
     st.subheader("내 기록 조회")
-    search_nickname = st.text_input("조회할 닉네임", placeholder=" ", key="search_nickname")
+    search_nickname = st.text_input("조회할 닉네임", placeholder="성적 확인 시 필요합니다.", key="search_nickname")
 
     if st.button("기록 조회"):
         if not search_nickname.strip():
